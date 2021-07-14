5bd8d704bf3d8a2a9e970836931ef135c0cc8bc3
Claireohan <86350730+Claireohan@users.noreply.github.com>
2021-07-14 08:19:51 +0100

    Added alternate start page just to illustrate service could potentially cover more than 1 action

--- app/routes.js
+++ app/routes.js
@@ -3,21 +3,5 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-// Run this code when a form is submitted to 'juggling-balls-answer'
-router.post('/juggling-balls-answer', function (req, res) {
-
-  // Make a variable and give it the value from 'juggling-balls'
-  var jugglingBalls = req.session.data['juggling-balls']
-
-  // Check whether the variable matches a condition
-  if (jugglingBalls == "3"){
-    // Send user to next page
-    res.redirect('/s12-date')
-  } else {
-    // Send user to ineligible page
-    res.redirect('/compliance-section-11')
-  }
-
-})
 
 module.exports = router
