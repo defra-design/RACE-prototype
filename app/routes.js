225b1a8c46fbfce1ba48f8d14bbac7a8fd3a5bee
John Sturniolo <john.sturniolo@gmail.com>
2022-10-31 11:54:03 +0000

    Continued building out journey for – No Gov Gateway or Defra ID accounts
    Started to build out the branching and routing of all the pages.
    
    For the following:
    Business or individual
    Org uk or not
    Individual-name
    Org in uk
    Company house yes
    Company confirm
    Company contact
    Company-check-info
    Company house no
    Sole trader
    Sole trader check info
    Charity region
    Charity name
    Charity address
    Select charity address
    Charity confirm
    Charity contact details
    Charity check info

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,80 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 
+
+// Run this code when a form is submitted to '/business-or-individual-answer'
+router.post('/defra-id-account/business-or-individual-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'business-or-individual'
+  var businessorindividual = req.session.data['business-or-individual']
+
+  // Check whether the variable matches a condition
+  if (businessorindividual == "A business or organisation"){
+    // Send user to next page
+    res.redirect('/defra-id-account/org-uk-or-not')
+  } else {
+    // Send user to individual-name
+    res.redirect('/defra-id-account/individual-name')
+  }
+
+})
+
+
+// Run this code when a form is submitted to '/defra-id-account/org-uk-or-not-answer'
+router.post('/defra-id-account/org-uk-or-not-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'Where is the organisation registered'
+  var whereisorgregistered = req.session.data['Where is the organisation registered']
+
+  // Check whether the variable matches a condition
+  if (whereisorgregistered == "UK"){
+    // Send user to next page
+    res.redirect('/defra-id-account/org-in-uk')
+  } else {
+    // Send user to individual-name
+    res.redirect('/defra-id-account/org-not-in-the-uk')
+  }
+
+})
+
+
+// Run this code when a form is submitted to '/defra-id-account/org-in-uk'
+router.post('/defra-id-account/org-in-uk-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'Is the organisation registered with companies-house'
+  var registeredwithcompanieshouse = req.session.data['registered-with-companies-house']
+
+  // Check whether the variable matches a condition
+  if (registeredwithcompanieshouse == "Yes, the organisation is registered at Companies House"){
+    // Send user to comp-house-yes
+    res.redirect('/defra-id-account/company-house-yes')
+  } else {
+    // Send user to comp-house-no
+    res.redirect('/defra-id-account/company-house-no')
+  }
+
+})
+
+
+// Run this code when a form is submitted to '/defra-id-account/company-house-no-answer'
+router.post('/defra-id-account/company-house-no-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'Are you are a sole trader or charity'
+  var areyouasoletraderorcharity = req.session.data['are-you-a-sole-trader-or-charity']
+
+  // Check whether the variable matches a condition
+  if (areyouasoletraderorcharity == "Sole trader"){
+    // Send user to sole-trader
+    res.redirect('/defra-id-account/sole-trader')
+  } else {
+    // Send user to charity-region
+    res.redirect('/defra-id-account/charity-region')
+  }
+
+})
+
+
 module.exports = router
+
+
+
