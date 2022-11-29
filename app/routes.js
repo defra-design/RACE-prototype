7dc634ad8dc23278c27f07eccca8d497760f6f9d
John Sturniolo <john.sturniolo@gmail.com>
2022-11-29 11:41:05 +0000

    Beta V1 updates
    Re-ordering folder structure
    Changing routing paths
    Deleting pages that are not needed

--- app/routes.js
+++ app/routes.js
@@ -10,7 +10,7 @@ const router = express.Router()
 // })
 
 // Run this code when a form is submitted to '/business-or-individual-answer'
-router.post('/defra-id-account/business-or-individual-answer', function (req, res) {
+router.post('/Beta/V1/defra-id-account/business-or-individual-answer', function (req, res) {
 
   // Make a variable and give it the value from 'business-or-individual'
   var businessorindividual = req.session.data['business-or-individual']
@@ -18,17 +18,17 @@ router.post('/defra-id-account/business-or-individual-answer', function (req, re
   // Check whether the variable matches a condition
   if (businessorindividual == "A business or organisation"){
     // Send user to next page
-    res.redirect('/defra-id-account/org-uk-or-not')
+    res.redirect('/Beta/V1/defra-id-account/org-uk-or-not')
   } else {
     // Send user to individual-name
-    res.redirect('/defra-id-account/individual-name')
+    res.redirect('/Beta/V1/defra-id-account/individual-name')
   }
 
 })
 
 
 // Run this code when a form is submitted to '/defra-id-account/org-uk-or-not-answer'
-router.post('/defra-id-account/org-uk-or-not-answer', function (req, res) {
+router.post('/Beta/V1/defra-id-account/org-uk-or-not-answer', function (req, res) {
 
   // Make a variable and give it the value from 'Where is the organisation registered'
   var whereisorgregistered = req.session.data['Where is the organisation registered']
@@ -36,17 +36,17 @@ router.post('/defra-id-account/org-uk-or-not-answer', function (req, res) {
   // Check whether the variable matches a condition
   if (whereisorgregistered == "UK"){
     // Send user to next page
-    res.redirect('/defra-id-account/org-in-uk')
+    res.redirect('/Beta/V1/defra-id-account/org-in-uk')
   } else {
     // Send user to individual-name
-    res.redirect('/defra-id-account/org-not-in-the-uk')
+    res.redirect('/Beta/V1/defra-id-account/org-not-in-the-uk')
   }
 
 })
 
 
 // Run this code when a form is submitted to '/defra-id-account/org-in-uk'
-router.post('/defra-id-account/org-in-uk-answer', function (req, res) {
+router.post('/Beta/V1/defra-id-account/org-in-uk-answer', function (req, res) {
 
   // Make a variable and give it the value from 'Is the organisation registered with companies-house'
   var registeredwithcompanieshouse = req.session.data['registered-with-companies-house']
@@ -54,17 +54,17 @@ router.post('/defra-id-account/org-in-uk-answer', function (req, res) {
   // Check whether the variable matches a condition
   if (registeredwithcompanieshouse == "Yes, the organisation is registered at Companies House"){
     // Send user to comp-house-yes
-    res.redirect('/defra-id-account/company-house-yes')
+    res.redirect('/Beta/V1/defra-id-account/company-house-yes')
   } else {
     // Send user to comp-house-no
-    res.redirect('/defra-id-account/company-house-no')
+    res.redirect('/Beta/V1/defra-id-account/company-house-no')
   }
 
 })
 
 
 // Run this code when a form is submitted to '/defra-id-account/company-house-no-answer'
-router.post('/defra-id-account/company-house-no-answer', function (req, res) {
+router.post('/Beta/V1/defra-id-account/company-house-no-answer', function (req, res) {
 
   // Make a variable and give it the value from 'Are you are a sole trader or charity'
   var areyouasoletraderorcharity = req.session.data['are-you-a-sole-trader-or-charity']
@@ -72,17 +72,17 @@ router.post('/defra-id-account/company-house-no-answer', function (req, res) {
   // Check whether the variable matches a condition
   if (areyouasoletraderorcharity == "Sole trader"){
     // Send user to sole-trader
-    res.redirect('/defra-id-account/sole-trader')
+    res.redirect('/Beta/V1/defra-id-account/sole-trader')
   } else {
     // Send user to charity-region
-    res.redirect('/defra-id-account/charity-region')
+    res.redirect('/Beta/V1/defra-id-account/charity-region')
   }
 
 })
 
 
 // Run this code when a form is submitted to '/defra-id-account/verify-identity-answer'
-router.post('/defra-id-account/verify-identity-answer', function (req, res) {
+router.post('/Beta/V1/defra-id-account/verify-identity-answer', function (req, res) {
 
   // Make a variable and give it the value from 'Verify identity'
   var verifyidentity = req.session.data['Verify identity']
@@ -90,16 +90,16 @@ router.post('/defra-id-account/verify-identity-answer', function (req, res) {
   // Check whether the variable matches a condition
   if (verifyidentity == "Send a code to xxxxxxxx478"){
     // Send user to verify-get-code
-    res.redirect('/defra-id-account/verify-get-code')
+    res.redirect('/Beta/V1/defra-id-account/verify-get-code')
   } else {
     // Send user to cverify-confirm-security-word
-    res.redirect('/defra-id-account/verify-confirm-security-word')
+    res.redirect('/Beta/V1/defra-id-account/verify-confirm-security-word')
   }
 
 })
 
 // Run this code when a form is submitted to '/submit-s12-statement/reservoir-details/change-reservoir-name-answer'
-router.post('/submit-s12-statement/reservoir-details/change-reservoir-name-answer', function (req, res) {
+router.post('/Beta/V1/submit-s12-statement/reservoir-details/change-name-answer', function (req, res) {
 
   // Make a variable and give it the value from 'changed-name'
   var changedname = req.session.data['changed-name']
@@ -107,16 +107,16 @@ router.post('/submit-s12-statement/reservoir-details/change-reservoir-name-answe
   // Check whether the variable matches a condition
   if (changedname == "Yes"){
     // Send user to new-reservoir-name
-    res.redirect('/submit-s12-statement/reservoir-details/new-name')
+    res.redirect('/Beta/V1/submit-s12-statement/reservoir-details/new-name')
   } else {
     // Send user to ?????
-    res.redirect('/submit-s12-statement/reservoir-details/confirm')
+    res.redirect('/Beta/V1/submit-s12-statement/reservoir-details/confirm')
   }
 
 })
 
 // Run this code when a form is submitted to '/submit-s12-statement/reservoir-details/change-operator-name-answer'
-router.post('/submit-s12-statement/reservoir-details/change-operator-name-answer', function (req, res) {
+router.post('/Beta/V1/submit-s12-statement/operator-details/change-name-answer', function (req, res) {
 
   // Make a variable and give it the value from 'operator-name'
   var operatorname = req.session.data['operator-name']
@@ -124,10 +124,10 @@ router.post('/submit-s12-statement/reservoir-details/change-operator-name-answer
   // Check whether the variable matches a condition
   if (operatorname == "Yes"){
     // Send user to change-operator-name-yes
-    res.redirect('/submit-s12-statement/operator-details/change-name-yes')
+    res.redirect('/Beta/V1/submit-s12-statement/operator-details/change-name-yes')
   } else {
     // Send user to confirm-details
-    res.redirect('/submit-s12-statement/operator-details/confirm')
+    res.redirect('/Beta/V1/submit-s12-statement/operator-details/confirm')
   }
 
 })
