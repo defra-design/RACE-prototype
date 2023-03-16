ba7a1ecfcafa5cdb39ec728bd6057f290a8255e1
John Sturniolo <john.sturniolo@gmail.com>
2023-03-16 13:46:25 +0000

    Update – Index of prototype
    Redesign of the information on the page.
    
    Consolidating features and different versions into clearly labelled sections.

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'RACE2',
+  serviceName: 'RACE 2',
 
   // Default port that prototype runs on
   port: '3000',
